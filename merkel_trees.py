df76436bcb89c4680f4bc0711aadb3757104f64f
Nick Sweeting <git@nicksweeting.com>
2019-03-21 01:29:22 -0400

    merkel changes

--- merkel_trees.py
+++ merkel_trees.py
@@ -1,11 +1,45 @@
+"""
+Take a bookmark archiver folder structure and turn it into a deduped
+merkel-tree blob index.
+
+For example:
+
+archive = {
+    'https://google.com': {
+        'index.html': 125,  # arbitrary bytes as file content
+        'js': {
+            'index.js': 34235,
+            'tracking.js': 2341,
+        },
+        'static': {
+            'test.mp3': 2353,
+            'alt': {
+                'duplicate_test.mp3': 2353,
+            }
+        },
+    },
+}
+
+Becomes:
+archive                                                      -> index/b17644f0ec1fc5f7b9ff7e7346b59aea
+archive/https://google.com                                   -> index/e75b0a87eaf354eb1ad950287e8bf716
+archive/https://google.com/index.html                        -> index/3def184ad8f4755ff269862ea77393dd
+archive/https://google.com/js                                -> index/033ff67f157230f984fbe5150b4b3dc9
+archive/https://google.com/js/index.js                       -> index/28a32c20769baa8373833005b125864b
+archive/https://google.com/js/tracking.js                    -> index/c8dfece5cc68249206e4690fc4737a8d
+archive/https://google.com/static                            -> index/1102530be9ed53fe4dcc41f72b5392bd
+archive/https://google.com/static/test.mp3                   -> index/23af4b45f1e166141a790d1a3126e77a  # same file as duplicate_test.mp3
+archive/https://google.com/static/alt                        -> index/b0dd67ebc9d1fe35dacd7382216348ff
+archive/https://google.com/static/alt/duplicate_test.mp3     -> index/23af4b45f1e166141a790d1a3126e77a  # notice how these two files point to the same hashed blob in the index
+"""
+
+import os
 import json
+
 from collections import defaultdict
 from hashlib import md5
 
-HASH_FUNC = md5
-
-def hash_hashes(hashes: list) -> str:
-    return HASH_FUNC(str(hashes).encode()).hexdigest()
+HASH_FUNC = md5   # sha256 is prob better but whatever
 
 
 def merkel_tree(obj: dict) -> dict:
@@ -21,18 +55,16 @@ def merkel_tree(obj: dict) -> dict:
             for key, val in obj.items()
         }
         return {
-            'hash': hash_hashes(sub_hashes.values()),
+            'hash': HASH_FUNC(str(sub_hashes.values()).encode()).hexdigest(),
             'subtree': sub_hashes,
         }
     return HASH_FUNC(str(obj).encode()).hexdigest()
-
 
 def verify_obj(obj: dict, hash_tree: dict) -> bool:
     """take an object and its merkel tree to verify the hashes match"""
     
     obj_hash_tree = merkel_tree(obj)
     return obj_hash_tree == hash_tree
-
 
 def flat_merkel_index(merkel_tree: dict) -> dict:
     """turn a nested merkel tree into a flat map {key1: hash, key2: hash}"""
@@ -45,7 +77,6 @@ def flat_merkel_index(merkel_tree: dict) -> dict:
             index[key] = val
     return index
 
-
 def reverse_flat_index(index: dict) -> dict:
     """reverse a map of {key: hash} into {hash: [keys]}"""
     reverse_index = defaultdict(set)
@@ -53,9 +84,38 @@ def reverse_flat_index(index: dict) -> dict:
         reverse_index[val].add(key)
 
     return {
-        key: list(val)
+        key: sorted(list(val))
         for key, val in reverse_index.items()
     }
+
+def symlink_file(original, link):
+    # if os.path.exists(link):
+    #     run(['rm', original])
+    #     print(f'{original.ljust(60)} -> {link} (already present)')
+    # else:
+    #     run(['mv', original, link])
+    #     print(f'{original.ljust(60)} -> {link}')
+    # run(['ln', '-s', link, original])
+
+    print(f'{original.ljust(60)} -> {link}')
+
+
+def symlink_merkel_tree(merkel_tree: dict, index_path: str, archive_path: str) -> None:
+    """
+    take an archive directory and move the files to an index based on their hash
+    then simlink original files to the hashed blobs in the index
+    """
+
+    symlink_path = os.path.join(index_path, merkel_tree['hash'])
+    symlink_file(archive_path, symlink_path)
+
+    for filename, hash_val in merkel_tree['subtree'].items():
+        original_path = os.path.join(archive_path, filename)
+        if isinstance(hash_val, str):
+            symlink_path = os.path.join(index_path, hash_val)
+            symlink_file(original_path, symlink_path)
+        elif isinstance(hash_val, dict):
+            symlink_merkel_tree(hash_val, index_path, original_path)
 
 
 def test_merkel(print_debug=True):
@@ -103,6 +163,8 @@ def test_merkel(print_debug=True):
         print(json.dumps(index, indent=4))
         print('---')
         print(json.dumps(reversed_index, indent=4))
+
+    symlink_merkel_tree(hash_tree, 'index', 'archive')
 
 if __name__ == '__main__':
     test_merkel()
